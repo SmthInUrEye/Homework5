c32bb8e28d5c2fd8f900f882e660b1c4b984b97f
SmthInUrEye <dragon-pg@yandex.ru>
2024-09-22 16:49:25 +0300

    Homework5

--- src/Main.java
+++ src/Main.java
@@ -22,65 +22,64 @@ public class Main {
             System.out.println ( "\n Для iOS — Установите облегченную версию приложения для iOS по ссылке" );
 
         // Задача №3
-        int year = 2028;
-        if ( (year > 1584) )
-            if ( (year % 4 == 0) && (year % 100 != 0) || (year % 400 ==0 ) )
-                System.out.println ("\n " + year + " год является високосным");
+        int year = 2031;
+        if ( ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) && (year > 1584) )
+            System.out.println ( "\n " + year + " год является високосным" );
         else
-                System.out.println ("\n " + year + " год не является високосным");
+            System.out.println ( "\n " + year + " год не является високосным" );
 
         // Задача №4
         int deliveryDistance = 95;
         int deliveryTime = 1;
         if ( deliveryDistance > 100 )
             System.out.println ( "Доставки свыше 100 км нет" );
-        else {
-            if ( deliveryDistance > 20 )
-                deliveryTime += 1;
-            if ( deliveryDistance > 60 )
-                deliveryTime += 1;
-            System.out.println ( "\n Потребуется дней для доставки : " + deliveryTime );
-        }
+        if ( deliveryDistance > 20 )
+            deliveryTime += 1;
+        if ( deliveryDistance > 60 )
+            deliveryTime += 1;
+        System.out.println ( "\n Потребуется дней для доставки : " + deliveryTime );
 
         // Задача №5
-        int monthNubmer = 4;
-        switch (monthNubmer) {
+        int monthNumber = 16;
+        switch (monthNumber) {
             case (12):
-                System.out.println ("\n Месяц принадлежит сезону зима ");
+                System.out.println ( "\n Месяц принадлежит сезону зима " );
                 break;
             case (1):
-                System.out.println ("\n Месяц принадлежит сезону зима ");
+                System.out.println ( "\n Месяц принадлежит сезону зима " );
                 break;
             case (2):
-                System.out.println ("\n Месяц принадлежит сезону зима ");
+                System.out.println ( "\n Месяц принадлежит сезону зима " );
                 break;
             case (3):
-                System.out.println ("\n Месяц принадлежит сезону весна ");
+                System.out.println ( "\n Месяц принадлежит сезону весна " );
                 break;
             case (4):
-                System.out.println ("\n Месяц принадлежит сезону весна ");
+                System.out.println ( "\n Месяц принадлежит сезону весна " );
                 break;
             case (5):
-                System.out.println ("\n Месяц принадлежит сезону весна ");
+                System.out.println ( "\n Месяц принадлежит сезону весна " );
                 break;
             case (6):
-                System.out.println ("\n Месяц принадлежит сезону лето ");
+                System.out.println ( "\n Месяц принадлежит сезону лето " );
                 break;
             case (7):
-                System.out.println ("\n Месяц принадлежит сезону лето ");
+                System.out.println ( "\n Месяц принадлежит сезону лето " );
                 break;
             case (8):
-                System.out.println ("\n Месяц принадлежит сезону лето ");
+                System.out.println ( "\n Месяц принадлежит сезону лето " );
                 break;
             case (9):
-                System.out.println ("\n Месяц принадлежит сезону осень ");
+                System.out.println ( "\n Месяц принадлежит сезону осень " );
                 break;
             case (10):
-                System.out.println ("\n Месяц принадлежит сезону осень ");
+                System.out.println ( "\n Месяц принадлежит сезону осень " );
                 break;
             case (11):
-                System.out.println ("\n Месяц принадлежит сезону осень ");
+                System.out.println ( "\n Месяц принадлежит сезону осень " );
                 break;
+            default:
+                System.out.println ( "Месяц указан не верно" );
         }
     }
 }
